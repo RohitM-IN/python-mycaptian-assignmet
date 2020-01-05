e55869d66c4ada0351fa1b548d9c3200f2797c1d
RootAndroid58 <rootand58@gmail.com>
2020-01-05 10:58:59 +0530

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,19 @@
+a = float(input("first number: "))
+b = float(input("second number: "))
+c = float(input("third number: "))
+if a > b:
+    if a < c:
+        median = a
+    elif b > c:
+        median = b
+    else:
+        median = c
+else:
+    if a > c:
+        median = a
+    elif b < c:
+        median = b
+    else:
+        median = c
+
+print("The median is", median)
